2242f2ecc488d4143a47fa24d26531f700ec6dba
Konstantin Davydov <davydovks@mail.ru>
2024-11-16 15:37:11 +0500

    write coder.edit

--- src/Controller/CoderController.php
+++ src/Controller/CoderController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Form\CoderType;
 use App\Repository\CoderRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -14,6 +17,32 @@ class CoderController extends AbstractController
     {
         return $this->render('coder/index.html.twig', [
             'coders' => $coderRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/coder/{id}', name: 'coder_edit')]
+    public function edit(
+        CoderRepository $coderRepository,
+        int $id,
+        Request $request,
+        EntityManagerInterface $entityManager
+    ): Response
+    {
+        $coder = $coderRepository->find($id);
+        $form = $this->createForm(CoderType::class, $coder);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $coder = $form->getData();
+            $entityManager->persist($coder);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('coder_index');
+        }
+
+        return $this->render('coder/edit.html.twig', [
+            'coder' => $coder,
+            'form' => $form
         ]);
     }
 }
