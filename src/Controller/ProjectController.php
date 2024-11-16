2a4d57ef32d8f7fcf488b3219e17a938c9357955
Konstantin Davydov <davydovks@mail.ru>
2024-11-16 22:40:25 +0500

    create project controller

---
+++ src/Controller/ProjectController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProjectRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ProjectController extends AbstractController
+{
+    #[Route('/project', name: 'project_index')]
+    public function index(ProjectRepository $projectRepository): Response
+    {
+        return $this->render('project/index.html.twig', [
+            'projects' => $projectRepository->findAll(),
+        ]);
+    }
+}
