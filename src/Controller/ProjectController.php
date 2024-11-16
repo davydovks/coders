7b1a90d2403359f9042a43cfcf48b74e3918501f
Konstantin Davydov <davydovks@mail.ru>
2024-11-17 00:20:41 +0500

    write project.edit route

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Form\ProjectType;
 use App\Repository\ProjectRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -14,6 +17,31 @@ class ProjectController extends AbstractController
     {
         return $this->render('project/index.html.twig', [
             'projects' => $projectRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/project/{id}/edit', name: 'project_edit')]
+    public function edit(
+        ProjectRepository $projectRepository,
+        int $id,
+        Request $request,
+        EntityManagerInterface $entityManager
+    ): Response {
+        $project = $projectRepository->find($id);
+        $form = $this->createForm(ProjectType::class, $project);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $project = $form->getData();
+            $entityManager->persist($project);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('project_index');
+        }
+
+        return $this->render('project/edit.html.twig', [
+            'project' => $project,
+            'form' => $form
         ]);
     }
 }
