065d75f4c16387cc586c4a8554d5aad029224256
Konstantin Davydov <davydovks@mail.ru>
2024-11-17 01:54:42 +0500

    add homepage

---
+++ src/Controller/MainController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CoderRepository;
+use App\Repository\ProjectRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/', name: 'homepage')]
+    public function index(): Response
+    {
+        return $this->render('main/index.html.twig');
+    }
+}
