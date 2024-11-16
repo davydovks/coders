9525def7b27e5d5595c2ca49030520c013326107
Konstantin Davydov <davydovks@mail.ru>
2024-11-17 00:42:38 +0500

    add projects list to coders pages

--- src/Form/CoderType.php
+++ src/Form/CoderType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Coder;
+use App\Entity\Project;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
@@ -24,6 +26,12 @@ class CoderType extends AbstractType
             ->add('phone', TelType::class, ['label' => 'Телефон'])
             ->add('birthdate', DateType::class, ['label' => 'Дата рождения'])
             ->add('fired', CheckboxType::class, ['label' => 'Уволен', 'required' => false])
+            ->add('projects', EntityType::class, [
+                'label' => 'Проекты',
+                'class' => Project::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+            ])
             ->add('save', SubmitType::class, ['label' => 'Сохранить'])
         ;
     }
